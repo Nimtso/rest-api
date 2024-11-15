06b7b91bfcf379ce6f64e13d7952ca0d4ef2a931
Nimtso <hadarnim@gmail.com>
2024-11-15 21:34:08 +0200

    Use correct post schema

--- src/db/models/posts.js
+++ src/db/models/posts.js
@@ -1,39 +1,17 @@
 const mongoose = require("mongoose");
 
-const postSchema = new mongoose.Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-      trim: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
-    author: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
-    tags: {
-      type: [String],
-      default: [],
-    },
-    isPublished: {
-      type: Boolean,
-      default: false,
-    },
-    publishedAt: {
-      type: Date,
-      default: null,
-    },
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  content: String,
+  owner: {
+    type: String,
+    required: true,
+  },
+});
 
-const Post = mongoose.model("Post", postSchema);
+const postModel = mongoose.model("Posts", postSchema);
 
-module.exports = Post;
+module.exports = postModel;
