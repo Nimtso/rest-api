d74a46db5c1b7b67b6fa9d4b26127b4ecb36f456
Nimtso <hadarnim@gmail.com>
2024-11-16 10:59:47 +0200

    Add logger and post route

---
+++ src/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const logger = require("./utils/logger");
+const routes = require("./routes/index");
+const loggerMiddleware = require("./middlewares/logger.js");
+const { connect: connectMongo } = require("../src/db/utils.js");
+
+const PORT = 3000;
+const URI_DB = "mongodb://localhost:27017";
+
+const createServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.use(loggerMiddleware);
+  app.use("/", routes);
+
+  return app;
+};
+
+const connectDB = async () => {
+  connectMongo(URI_DB);
+};
+
+const init = async () => {
+  await connectDB();
+  const app = createServer();
+
+  app.listen(PORT, () => {
+    logger.debug(`Listening on port ${PORT}`);
+  });
+};
+
+module.exports = { createServer, init };
